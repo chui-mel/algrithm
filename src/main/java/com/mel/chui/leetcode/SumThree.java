b8caf41abf982664de0b9fce1fe83ba2a617912c
Chui Melbourne <chui.melbourne@gmail.com>
2021-03-08 15:53:20 +1100

    Implement 3sum with O(n^2)

--- src/main/java/com/mel/chui/leetcode/SumThree.java
+++ src/main/java/com/mel/chui/leetcode/SumThree.java
@@ -1,7 +1,6 @@
 package com.mel.chui.leetcode;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * https://leetcode.com/problems/3sum/
@@ -11,39 +10,27 @@ public class SumThree {
 		Arrays.sort(nums);
 		List<List<Integer>> result = new ArrayList<>();
     for (int i = 0; i < nums.length - 2; i++) {
-			Map<Integer, Integer> map = new HashMap<>();
-			for (int j = i + 1; j < nums.length; j++) {
-				int remain = 0 - nums[i] - nums[j];
-				if (map.containsKey(remain)) {
-					List<Integer> oneGroup = List.of(nums[i], nums[j], remain);
-					result.add(oneGroup);
-				}
-				map.put(nums[j], j);
+    	if (i >= 1 && nums[i] == nums[i-1]) {
+    		continue;
 			}
-		}
 
-		return result.stream().distinct().collect(Collectors.toList());
-	}
-
-	public List<List<Integer>> sumThreeBruceForce(int[] nums) {
-		List<List<Integer>> result = new ArrayList<>();
-
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				for (int k = j + 1; k < nums.length; k++) {
-					if ((nums[i] + nums[j] + nums[k] == 0)
-							&& !containsTriplet(result, List.of(nums[i], nums[j], nums[k]))) {
-						result.add(List.of(nums[i], nums[j], nums[k]));
-					}
+    	int left = i + 1, right = nums.length - 1;
+    	boolean loop = true;
+    	while (left < right && loop) {
+				if (nums[left] + nums[right] + nums[i] == 0) {
+					result.add(List.of(nums[i], nums[left], nums[right]));
+					while (left < right && nums[left] == nums[left + 1]) left += 1;
+					while (left < right && nums[right] == nums[right - 1]) right -= 1;
+					left += 1;
+					right -= 1;
+				} else if (nums[left] + nums[right] + nums[i] > 0) {
+					right -= 1;
+				} else {
+					left += 1;
 				}
 			}
 		}
+
 		return result;
-	}
-
-	private boolean containsTriplet(List<List<Integer>> source, List<Integer> data) {
-		return source.stream()
-				.filter( l -> l.containsAll(data) && data.containsAll(l))
-				.count() != 0;
 	}
 }
